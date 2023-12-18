2de863738ae025863048192ce4ce68d4b080428b
Lam <phamlam92@gmail.com>
2023-12-18 16:08:58 -0600

    Add(-): WIP Header profile avatar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div>
+      <div className="w-full">
+        header
+        <div>- menu items</div>
+      </div>
+      <Image
+        className="w-72 h-72 rounded-full"
+        src="/vercel.svg"
+        alt="Rounded avatar"
+        width={72}
+        height={72}
+      />
+    </div>
+  );
 };
 
 export default Header;
--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+// Components
+import Header from "../Header";
+
 const Portfolio = () => {
-  return <div>portfolio</div>;
+  return (
+    <div className="h-screen w-full">
+      <Header />
+      <div>about me</div>
+      <div>contact me</div>
+    </div>
+  );
 };
 
 export default Portfolio;
