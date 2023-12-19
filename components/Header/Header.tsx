21702714fe65303588aaeb7eceb5bc3c19617047
Lam <phamlam92@gmail.com>
2023-12-18 18:13:08 -0600

    Add(-): WIP avatar rounded border

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div>
-      <div className="w-full">
+      <div className="w-full h-72">
         header
         <div>- menu items</div>
       </div>
       <Image
-        className="w-72 h-72 rounded-full"
+        className="w-72 h-72 rounded-full ring-2 ring-gray-300"
         src="/vercel.svg"
         alt="Rounded avatar"
         width={72}
